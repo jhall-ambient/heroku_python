63beae18d814631cff76a0bcfdcf1a7e2fdc54e1
Jason Hall <jhall@ambientdevices.com>
2012-06-07 13:37:50 -0400

    Fixed typo in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World, from Python!'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
